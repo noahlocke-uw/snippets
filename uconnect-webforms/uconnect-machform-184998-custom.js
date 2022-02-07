fb355862f96a45ebe8796cb8fb80dd947accda2e
Noah Locke <62952615+noahlocke-uw@users.noreply.github.com>
2022-02-07 08:54:55 -0600

    Update uconnect-machform-184998-custom.js

--- uconnect-webforms/uconnect-machform-184998-custom.js
+++ uconnect-webforms/uconnect-machform-184998-custom.js
@@ -1,15 +1,18 @@
-document.querySelector("#li_22").addEventListener("change", (event) => {
+window.onload=function(){
+  document.querySelector("#li_22").addEventListener("change", (event) => {
   if (document.querySelectorAll('input[type="checkbox"]:checked').length > 1) {
     showCustomAlert();
   } else {
     hideCustomAlert();
   }
-});
+  });
 
-function showCustomAlert() {
-  document.querySelector("#li_25").style.display = "inline";
+  function showCustomAlert() {
+    document.querySelector("#li_25").style.display = "inline";
+  }
+
+  function hideCustomAlert() {
+    document.querySelector("#li_25").style.display = "none";
+  }
 }
 
-function hideCustomAlert() {
-  document.querySelector("#li_25").style.display = "none";
-}
