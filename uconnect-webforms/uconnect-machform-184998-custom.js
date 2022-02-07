30c453f7b53e4508ef2f897759e4c11f832575f0
Noah Locke <62952615+noahlocke-uw@users.noreply.github.com>
2022-02-07 08:44:33 -0600

    Create uconnect-machform-184998-custom.js

---
+++ uconnect-webforms/uconnect-machform-184998-custom.js
@@ -0,0 +1,15 @@
+function showCustomAlert() {
+  document.querySelector("#li_25").style.display = "inline";
+}
+
+function hideCustomAlert() {
+  document.querySelector("#li_25").style.display = "none";
+}
+
+document.querySelector("#li_22").addEventListener("change", (event) => {
+  if (document.querySelectorAll('input[type="checkbox"]:checked').length > 1) {
+    showCustomAlert();
+  } else {
+    hideCustomAlert();
+  }
+});
