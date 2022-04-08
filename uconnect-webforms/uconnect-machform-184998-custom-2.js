da7044f8d46f38544b5030efd8d1605c286bb96c
Noah Locke <62952615+noahlocke-uw@users.noreply.github.com>
2022-04-08 15:20:06 -0500

    Refactor

--- uconnect-webforms/uconnect-machform-184998-custom-2.js
+++ uconnect-webforms/uconnect-machform-184998-custom-2.js
@@ -16,18 +16,11 @@ window.onload=function(){
   });
   
   // prevent spaces in PIN
-  $(function() {
-    $("input#element_12").on({
-       keydown: function(e) {
-          if (e.which === 32)
-             return false;
-       },
-       // When spaces managed to "sneak in" via copy/paste
-       change: function() {
-          // Regex-remove all spaces in the final value
-          this.value = this.value.replace(/\s/g, "");
-       }
-    });
+  document.querySelector("#element_12").addEventListener('keypress', function ( event ) {  
+   var key = event.keyCode;
+    if (key === 32) {
+      event.preventDefault();
+    }
   });
 }
 
