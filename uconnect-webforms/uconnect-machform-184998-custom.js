9f39f59c70990507583b70d05989ed1aae397361
Noah Locke <62952615+noahlocke-uw@users.noreply.github.com>
2022-04-08 14:58:58 -0500

    Keeping spaces out of copy/paste

--- uconnect-webforms/uconnect-machform-184998-custom.js
+++ uconnect-webforms/uconnect-machform-184998-custom.js
@@ -17,8 +17,16 @@ window.onload=function(){
   
   // prevent spaces in PIN
   $(function() {
-    $("input#element_12").on("keydown", function (e) {
-        return e.which !== 32;
+    $("input#element_12").on({
+       keydown: function(e) {
+          if (e.which === 32)
+             return false;
+       },
+       // When spaces managed to "sneak in" via copy/paste
+       change: function() {
+          // Regex-remove all spaces in the final value
+          this.value = this.value.replace(/\s/g, "");
+       }
     });
   });
 }
